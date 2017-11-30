d738102a999fe044cdc3a84fdc16d4ed16d9dd31
Braun <tenzin.braun@gmail.com>
2017-11-30 22:04:19 +0100

    [IHM] ihm release 2.0

--- QCMIUTLyon1/app/src/main/java/fr/iutmindfuck/qcmiutlyon1/AuthActivity.java
+++ QCMIUTLyon1/app/src/main/java/fr/iutmindfuck/qcmiutlyon1/AuthActivity.java
@@ -1,8 +1,10 @@
 package fr.iutmindfuck.qcmiutlyon1;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 import fr.iutmindfuck.qcmiutlyon1.handlers.UserSQLHandler;
@@ -15,6 +17,7 @@ public class AuthActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_auth);
 
         userSQLHandler = new UserSQLHandler(new SQLServices(this));
@@ -27,12 +30,12 @@ public class AuthActivity extends AppCompatActivity {
         if (userSQLHandler.isPasswordCorrectFor(username, password)) {
             if (userSQLHandler.isTeacher(username)) {
                 // TODO: Redirect Prof Panel & remove setText
-                ((TextView)findViewById(R.id.auth_error)).setText("PROF");
+                ((TextView)findViewById(R.id.auth_error)).setText(getResources().getText(R.string.set_prof));
             }
             else
             {
                 // TODO: Redirect Prof User & remove setText
-                ((TextView)findViewById(R.id.auth_error)).setText("USER");
+                ((TextView)findViewById(R.id.auth_error)).setText(getResources().getText(R.string.set_user));
             }
         }
         else
