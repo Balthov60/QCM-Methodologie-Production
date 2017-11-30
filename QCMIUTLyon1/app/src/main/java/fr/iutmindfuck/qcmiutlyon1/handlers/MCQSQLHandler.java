3a51500b7f9ab633b3a3e694f34de2d7381f8894
Balthov60 <Balthazarfrolin@gmail.com>
2017-11-30 14:44:07 +0100

    Add MSC SQL Handling

---
+++ QCMIUTLyon1/app/src/main/java/fr/iutmindfuck/qcmiutlyon1/handlers/MCQSQLHandler.java
@@ -0,0 +1,64 @@
+package fr.iutmindfuck.qcmiutlyon1.handlers;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import fr.iutmindfuck.qcmiutlyon1.data.MCQ;
+import fr.iutmindfuck.qcmiutlyon1.services.SQLServices;
+
+public class MCQSQLHandler {
+
+    private static final String MCQ_TABLE = "MCQ";
+    private static final String MCQ_ID = "idMCQ";
+    private static final String MCQ_NAME = "name";
+    private static final String MCQ_DESCRIPTION = "name";
+    private static final String MCQ_TYPE = "type";
+    private static final String MCQ_COEF = "coef";
+
+    private SQLServices sqlServices;
+
+    public MCQSQLHandler(SQLServices sqlServices) {
+        this.sqlServices = sqlServices;
+    }
+
+    public static String getSQLForMCQTableCreation() {
+        return "CREATE TABLE " + MCQ_TABLE + "(" +
+                MCQ_ID + " varchar(32) PRIMARY KEY, " +
+                MCQ_NAME + " varchar(64), " +
+                MCQ_DESCRIPTION + " varchar(256), " +
+                MCQ_TYPE + " varchar(7), " +
+                MCQ_COEF + " float)";
+    }
+    public static String getSQLForMCQTableSuppression() {
+        return "DROP TABLE IF EXISTS " + MCQ_TABLE;
+    }
+
+    public MCQ getMCQ(String idMCQ) {
+        Cursor cursor = sqlServices.getData(MCQ_TABLE, null,
+                                      MCQ_ID + " = ?", new String[] {idMCQ});
+
+        if (!cursor.moveToFirst()) {
+            cursor.close();
+            return null;
+        }
+        MCQ mcq = new MCQ(cursor.getString(cursor.getColumnIndex(MCQ_ID)),
+                          cursor.getString(cursor.getColumnIndex(MCQ_NAME)),
+                          cursor.getString(cursor.getColumnIndex(MCQ_DESCRIPTION)),
+                          cursor.getString(cursor.getColumnIndex(MCQ_TYPE)),
+                          cursor.getFloat(cursor.getColumnIndex(MCQ_COEF)));
+
+        cursor.close();
+        return mcq;
+    }
+
+    public void createOrReplaceMCQ(MCQ mcq) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MCQ_ID, mcq.getId());
+        contentValues.put(MCQ_NAME, mcq.getName());
+        contentValues.put(MCQ_DESCRIPTION, mcq.getDescription());
+        contentValues.put(MCQ_TYPE, mcq.getType());
+        contentValues.put(MCQ_COEF, mcq.getCoef());
+
+        sqlServices.createOrReplaceData(MCQ_TABLE, contentValues);
+    }
+}
