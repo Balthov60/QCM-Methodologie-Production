fc698cf4fa461af2078ec64ec20727153de0af7e
Balthov60 <Balthazarfrolin@gmail.com>
2017-12-08 14:35:20 +0100

    Clean Code

--- QCMIUTLyon1/app/src/main/java/fr/iutmindfuck/qcmiutlyon1/MCQListActivity.java
+++ QCMIUTLyon1/app/src/main/java/fr/iutmindfuck/qcmiutlyon1/MCQListActivity.java
@@ -5,41 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import fr.iutmindfuck.qcmiutlyon1.data.MCQ;
 import fr.iutmindfuck.qcmiutlyon1.handlers.MCQSQLHandler;
 import fr.iutmindfuck.qcmiutlyon1.services.SQLServices;
 
-/**
- * Created by sntri on 04/12/2017.
- */
 
 public class MCQListActivity extends AppCompatActivity {
 
     private ListView mListView;
-    private MCQSQLHandler mcqsqlHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mcq_list);
-        mListView = (ListView) findViewById(R.id.mcq_list_view);
-        mcqsqlHandler =new MCQSQLHandler(new SQLServices(MCQListActivity.this));
-        mcqsqlHandler.createOrReplaceMCQ(new MCQ(0,"Loi Exponentielle", "C'est un putain de test super super super super super super super super super super long",false, 1.5f ));
-        List<MCQ> mcqList = generateMCQ();
 
-        MCQListAdapter adapter = new MCQListAdapter(MCQListActivity.this, mcqList);
-        mListView.setAdapter(adapter);
+        MCQSQLHandler mcqSQLHandler = new MCQSQLHandler(new SQLServices(this));
+        mListView = findViewById(R.id.mcq_list_view);
+
+        mListView.setAdapter(new MCQListAdapter(MCQListActivity.this,
+                                                mcqSQLHandler.getMCQs()));
     }
 
-    private List<MCQ> generateMCQ(){
-        List<MCQ> mcqList = this.mcqsqlHandler.getMCQs();
-        return mcqList;
-    }
-
-    public void onModifyMCQButtonClick(View view)
+    public void onModifyMCQButtonClick(View v)
     {
 
     }
--- QCMIUTLyon1/app/src/main/java/fr/iutmindfuck/qcmiutlyon1/MCQListAdapter.java
+++ QCMIUTLyon1/app/src/main/java/fr/iutmindfuck/qcmiutlyon1/MCQListAdapter.java
@@ -1,6 +1,7 @@
 package fr.iutmindfuck.qcmiutlyon1;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,42 +12,40 @@ import java.util.List;
 
 import fr.iutmindfuck.qcmiutlyon1.data.MCQ;
 
-/**
- * Created by sntri on 04/12/2017.
- */
 public class MCQListAdapter extends ArrayAdapter<MCQ> {
 
-    public MCQListAdapter(Context context, List<MCQ> mcqList){
+    MCQListAdapter(Context context, List<MCQ> mcqList) {
         super(context, 0, mcqList);
     }
 
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        if(convertView == null){
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_row_item_mcq,parent, false);
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.mcq_list_item,
+                                                                    parent, false);
         }
 
         MCQViewHolder viewHolder = (MCQViewHolder) convertView.getTag();
-        if(viewHolder == null){
-            viewHolder = new MCQViewHolder();
-            viewHolder.id = (TextView) convertView.findViewById(R.id.id_mcq);
-            viewHolder.mcq_name = (TextView) convertView.findViewById(R.id.mcq_name);
+        if(viewHolder == null) {
+            viewHolder = new MCQViewHolder(convertView);
             convertView.setTag(viewHolder);
         }
 
-        //getItem(position) va récupérer l'item [position] de la List<Tweet> tweets
         MCQ mcq = getItem(position);
-
-        //il ne reste plus qu'à remplir notre vue
-        viewHolder.id.setText("QCM " + mcq.getName());
-        viewHolder.mcq_name.setText(mcq.getDescription());
+        viewHolder.mcqNameView.setText(mcq != null ? mcq.getName() : null);
+        viewHolder.mcqDescriptionView.setText(mcq != null ? mcq.getDescription() : null);
 
         return convertView;
     }
 
-    private class MCQViewHolder{
-        public TextView id;
-        public TextView mcq_name;
+    class MCQViewHolder {
+        TextView mcqNameView;
+        TextView mcqDescriptionView;
+
+        MCQViewHolder(View convertView) {
+            mcqNameView = convertView.findViewById(R.id.mcq_list_item_name);
+            mcqDescriptionView = convertView.findViewById(R.id.mcq_name);
+        }
     }
 }
